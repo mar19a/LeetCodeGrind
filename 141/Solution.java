3eed392fe3d144602044a7088ef0e62b0bfe7307
Mariano Majano Amaya <mariano@bu.edu>
2024-06-13 00:02:13 -0400

    141 solution

--- 141/Solution.java
+++ 141/Solution.java
@@ -4,8 +4,10 @@ public class Solution {
         ListNode fast = head;
         ListNode slow = head;
         while (fast != null && fast.next != null) {
+            
             fast = fast.next.next;
             slow = slow.next;
+
             if (fast == slow) return true;
         }
         return false;
